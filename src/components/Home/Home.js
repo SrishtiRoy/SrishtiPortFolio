73c36fc63f70e21914a003b4dfc19219e4ffc829
Srishti <srishti_roy.choudhury@bp.com>
2025-07-22 23:31:33 +0530

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-girl.png";
+import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 
@@ -8,6 +9,7 @@ function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
+        <Particle />
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
@@ -20,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> srishti BEHERA</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
